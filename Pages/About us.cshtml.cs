beb0d5557ec1452aae1eadb08e26b1dc3601281d
LeonardduPlessis <leonardduplessis7777@icloud.com>
2023-08-04 10:20:50 +0200

    Initial commit

---
+++ Pages/About us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComicBookStore.Pages.Shared
+{
+    public class About_usModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Favorites.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ComicBookStore.wwwroot
+{
+    public class FavoritesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
